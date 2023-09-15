418a6523bd6b7552bf07ee250160f130435c1835
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-09-15 12:46:57 +0530

    a

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-let http = require('http');
+const express = require('express');
+const app = express();
 let port = 2819;
-let data = require('./main');
-let server = http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    const result = data.createElement();
-    res.write(result);
-    res.end();
-})
 
-server.listen(port,(err)=>{
+let data = require('./main');
+
+app.listen(port,(err)=>{
     if(err){
         console.log(`something went wrong ${err}`);
     }else{
@@ -16,3 +12,9 @@ server.listen(port,(err)=>{
     }
 })
 
+app.get('/gettime',function(req,res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    const result = data.createElement();
+    res.write(result);
+    res.end();
+})
